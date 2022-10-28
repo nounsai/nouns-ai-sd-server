306cd295b6aaf499b0e12d5454364ff240a2fb75
Eric Olszewski <eolszewski@gmail.com>
2022-10-28 09:47:00 -0500

    Add live server config.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Nouns AI SD Server
 
 More to come to this page, soon!
+
+`gunicorn --bind 0.0.0.0:5000 wsgi:app --timeout 90`
--- server.py
+++ server.py
@@ -92,5 +92,5 @@ def get_image():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0')
+    # app.run(debug=True)
+    app.run(host='0.0.0.0')
